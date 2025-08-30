a5bf4e9dce0d6bd460b482e20b3d8239b6657b9e
Dusan Turajlic <dusan.turajlic@cambri.io>
2025-08-30 11:39:52 +0300

    Adds providers

---
+++ app/src/providers/base.ts
@@ -0,0 +1,6 @@
+export default abstract class BaseProvider {
+    abstract get<T>(path: string): Promise<T>;
+    abstract create<T>(path: string, data: T): Promise<T & {id: string}>;
+    abstract update<T>(path: string, data: Partial<T>): Promise<T>;
+    abstract delete(path: string): Promise<void>;
+}
