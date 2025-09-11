87d4bdf91f6967ec903c5e45c6426229cf3b650e
Dusan Turajlic <dusan.turajlic@cambri.io>
2025-09-11 22:07:28 +0300

    Changes function name

--- app/src/utils/uuid.ts
+++ app/src/utils/uuid.ts
@@ -5,16 +5,16 @@ import { v3 as uuidv3 } from 'uuid';
 // This should never change
 const UUID_DATE_NAMESPACE = '5f50d310-7a34-43fb-9985-507fee938089';
 
-function createSafeDateString(date: Date) {
+function createSafeDate(date: Date) {
     return date.toISOString().split('T');
 }
 
 export function createDateUUID(date: Date) {
-    const [dateString] = createSafeDateString(date);
+    const [dateString] = createSafeDate(date);
     return uuidv3(dateString, UUID_DATE_NAMESPACE);
 }
 
 export function createDateTimeUUID(date: Date, namespace: string) {
-    const [, timeString] = createSafeDateString(date);
+    const [, timeString] = createSafeDate(date);
     return uuidv3(timeString, namespace);
 }
