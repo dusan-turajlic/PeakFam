f4702c904962d2c9ed9c03b6a5aaecdb8590303d
Dusan Turajlic <dusan.turajlic@cambri.io>
2025-09-12 23:47:38 +0300

    Adds menu bar and dialog

---
+++ app/src/components/TabMenu.tsx
@@ -0,0 +1,66 @@
+import { loggerDialog } from "@/atoms/loggerDialog";
+import {
+    PlusIcon,
+    EllipsisHorizontalCircleIcon,
+    RectangleGroupIcon,
+    ScaleIcon,
+    FireIcon
+} from "@heroicons/react/24/outline";
+import { Link } from "react-router-dom";
+import { useSetAtom } from "jotai";
+
+export default function TabMenu() {
+    const setOpen = useSetAtom(loggerDialog);
+    return (
+        <>
+            <div className="h-20" />
+            <div className="fixed bottom-0 left-0 right-0 h-20">
+                <nav className="w-full bg-gray-800 border-t border-gray-500 text-white flex justify-center items-center pb-6 pt-3 px-3">
+                    <ul className="grid grid-cols-5 gap-4 w-full justify-center items-center text-center text-xs relative">
+                        <li>
+                            <Link to="/" className="ui-selected:text-white ui-not-selected:text-neutral-400 focus:outline-none">
+                                <div className="flex flex-col items-center gap-1">
+                                    <RectangleGroupIcon className="h-7 w-7" aria-hidden="true" />
+                                    <span>Dashboard</span>
+                                </div>
+                            </Link>
+                        </li>
+
+                        <li>
+                            <Link to="/" className="ui-selected:text-white ui-not-selected:text-neutral-400 focus:outline-none">
+                                <div className="flex flex-col items-center gap-1">
+                                    <FireIcon className="h-7 w-7" aria-hidden="true" />
+                                    <span>Food Log</span>
+                                </div>
+                            </Link>
+                        </li>
+
+                        <li>
+                            <button type="button" onClick={() => setOpen(true)} className="block -mt-8 rounded-full bg-white w-16 h-16 flex items-center justify-center m-auto shadow-lg ring-1 ring-neutral-300">
+                                <PlusIcon className="h-8 w-8 text-black" aria-hidden="true" />
+                            </button>
+                        </li>
+
+                        <li>
+                            <Link to="/" className="ui-selected:text-white ui-not-selected:text-neutral-400 focus:outline-none">
+                                <div className="flex flex-col items-center gap-1">
+                                    <ScaleIcon className="h-7 w-7" aria-hidden="true" />
+                                    <span>Strategy</span>
+                                </div>
+                            </Link>
+                        </li>
+
+                        <li>
+                            <Link to="/more" className="ui-selected:text-white ui-not-selected:text-neutral-400 focus:outline-none">
+                                <div className="flex flex-col items-center gap-1">
+                                    <EllipsisHorizontalCircleIcon className="h-7 w-7" aria-hidden="true" />
+                                    <span>More</span>
+                                </div>
+                            </Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </>
+    );
+}
---
+++ app/src/components/__test__/FoodLoggerDialog.spec.tsx
@@ -0,0 +1,49 @@
+import { render, screen } from "@testing-library/react";
+import { expect, it, describe, beforeEach } from "vitest";
+import FoodLoggerDialog from "../FoodLoggerDialog";
+import { createStore, Provider } from "jotai";
+import { loggerDialog } from "@/atoms/loggerDialog";
+
+const FoodLoggerDialogWithProvider = ({ store }: { store: ReturnType<typeof createStore> }) => (
+    <Provider store={store}>
+        <FoodLoggerDialog />
+    </Provider>
+);
+
+describe('FoodLoggerDialog', () => {
+    let store: ReturnType<typeof createStore>;
+    beforeEach(() => {
+        store = createStore()
+        store.set(loggerDialog, true)
+    });
+    it('renders dialog when open', async () => {
+        render(<FoodLoggerDialogWithProvider store={store} />);
+
+        expect(screen.getByRole('dialog')).toBeInTheDocument();
+    });
+
+    it('renders dialog title', () => {
+        render(<FoodLoggerDialogWithProvider store={store} />);
+
+        expect(screen.getByRole('heading', { name: 'Quick Actuons' })).toBeInTheDocument();
+    });
+
+    it('renders action buttons', () => {
+        render(<FoodLoggerDialogWithProvider store={store} />);
+
+        const buttons = screen.getAllByRole('button');
+        expect(buttons).toHaveLength(2);
+
+        const [scanButton, quickAddButton] = buttons;
+        // Check button content
+        expect(scanButton).toHaveTextContent('Scan Item');
+        expect(quickAddButton).toHaveTextContent('Quick Add');
+    });
+
+    it('does not render when closed', () => {
+        store.set(loggerDialog, false)
+        render(<FoodLoggerDialogWithProvider store={store} />);
+
+        expect(screen.queryByTestId('dialog')).not.toBeInTheDocument();
+    });
+});
